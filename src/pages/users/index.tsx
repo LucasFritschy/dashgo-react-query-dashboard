bb07fd6ccae5480d761c208aef3161122ba98e4b
LucasFritschy <lucasfritschy@gmail.com>
2021-05-31 18:30:15 -0300

    config axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,6 +22,7 @@ import { useQuery } from 'react-query'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
+import { api } from '../../services/api'
 
 type User = {
   id: string
@@ -34,8 +35,7 @@ export default function UserList() {
   const { data, isLoading, error, isFetching } = useQuery(
     'users',
     async () => {
-      const response = await fetch('http://localhost:3000/api/users')
-      const data = await response.json()
+      const { data } = await api.get('/users')
 
       const users: User[] = data.users.map((user) => {
         return {
